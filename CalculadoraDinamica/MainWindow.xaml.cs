c20d8d24e928ab897f649576856c7b51e17bf453
Ruben9911 <ruben.fortes-marques@iesdoctorbalmis.com>
2021-10-14 12:51:44 +0200

    Agregar archivos de proyecto.

---
+++ CalculadoraDinamica/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculadoraDinamica
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {        
+        public MainWindow()
+        {
+            InitializeComponent();
+            CrearBotones();
+        }
+        public void CrearBotones()
+        {
+            int contador = 1;
+            for (int contadorFilas = 1; contadorFilas < 4; contadorFilas++)
+            {
+                for (int contadorColumnas = 0; contadorColumnas < 3; contadorColumnas++, contador++)
+                {
+                    Button b = new Button
+                    {
+                        Content = contador,
+                        Style = (Style)this.Resources["botones"],
+
+                    };
+                    Grid.SetColumn(b, contadorColumnas);
+                    Grid.SetRow(b, contadorFilas);
+                    grid.Children.Add(b);
+                }
+            }
+        }
+    }
+}
